7e13a6004cdef14022af7a03b3b7b541b6d9fc17
xialeistudio <xialeistudio@gmail.com>
2016-06-26 04:44:15 +0800

    表格渲染

--- src/com/ddhigh/mybatis/window/Dashboard.java
+++ src/com/ddhigh/mybatis/window/Dashboard.java
@@ -8,9 +8,14 @@ import com.ddhigh.mybatis.worker.MemoryMonitorWorker;
 import org.apache.log4j.Logger;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
+import java.util.Vector;
 
 public class Dashboard {
     private static Logger logger = Logger.getLogger(Dashboard.class);
@@ -25,6 +30,7 @@ public class Dashboard {
     private JButton btnRefreshTable;
     private JButton btnGenerate;
     private JTable tableTable;
+    private JTextField txtEntity;
     private DbUtil dbUtil;
 
     //生成参数区域
@@ -32,6 +38,7 @@ public class Dashboard {
     private String modelPkg;
     private String mapPkg;
     private String daoPkg;
+    private String entitySuffix = "Entity";
 
     public Dashboard(final DbUtil dbUtil) {
         this.dbUtil = dbUtil;
@@ -57,6 +64,26 @@ public class Dashboard {
             @Override
             public void actionPerformed(ActionEvent e) {
                 launchTableLoader();
+            }
+        });
+
+        txtEntity.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                entitySuffix = txtEntity.getText().trim();
+                logger.info("entitySuffix => " + entitySuffix);
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                entitySuffix = txtEntity.getText().trim();
+                logger.info("entitySuffix => " + entitySuffix);
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                entitySuffix = txtEntity.getText().trim();
+                logger.info("entitySuffix => " + entitySuffix);
             }
         });
         //启动监控线程
@@ -93,6 +120,7 @@ public class Dashboard {
                 btnRefreshTable.setEnabled(true);
                 btnGenerate.setEnabled(true);
                 labelStatus.setText("成功加载【" + list.size() + "】张数据表");
+                displayTableWithData(list);
             }
 
             @Override
@@ -103,5 +131,89 @@ public class Dashboard {
             }
         });
         getTablesWorker.execute();
+    }
+
+    private static Vector<String> tableColumnNames = new Vector<>();
+
+    static {
+        tableColumnNames.add("序号");
+        tableColumnNames.add("表名");
+        tableColumnNames.add("实体类名");
+    }
+
+    /**
+     * 通过数据加载table
+     *
+     * @param list
+     */
+    private void displayTableWithData(final List<TableEntity> list) {
+        for (TableEntity t : list) {
+            t.setEntityName(t.getEntityName() + entitySuffix);
+        }
+        tableTable.setModel(new TableModel() {
+            @Override
+            public int getRowCount() {
+                return list.size();
+            }
+
+            @Override
+            public int getColumnCount() {
+                return tableColumnNames.size();
+            }
+
+            @Override
+            public String getColumnName(int columnIndex) {
+                return tableColumnNames.get(columnIndex);
+            }
+
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                return String.class;
+            }
+
+            @Override
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return columnIndex == 2;
+            }
+
+            @Override
+            public Object getValueAt(int rowIndex, int columnIndex) {
+                TableEntity entity = list.get(rowIndex);
+                if (columnIndex == 0) {
+                    return rowIndex + 1;
+                }
+                if (columnIndex == 1) {
+                    return entity.getTableName();
+                }
+                if (columnIndex == 2) {
+                    return entity.getEntityName();
+                }
+                return null;
+            }
+
+            @Override
+            public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+                if (columnIndex == 2) {
+                    TableEntity entity = list.get(rowIndex);
+                    entity.setEntityName(aValue.toString());
+                    logger.debug("[" + rowIndex + "][" + columnIndex + "] - " + aValue);
+                }
+            }
+
+            @Override
+            public void addTableModelListener(TableModelListener l) {
+
+            }
+
+            @Override
+            public void removeTableModelListener(TableModelListener l) {
+
+            }
+        });
+        TableColumn tableColumn = tableTable.getColumnModel().getColumn(0);
+        tableColumn.setMaxWidth(48);
+        tableColumn.setPreferredWidth(48);
+        tableColumn.setMinWidth(48);
+        tableTable.validate();
     }
 }
